f40624194a8056cd65cc358230ae86b7366a00ed
Justin <sumbhodi1618@gmail.com>
2019-05-22 08:53:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,16 +66,16 @@ def adding_matz
 
 	programmer_hash = 
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
+        grace_hopper => {
+          known_for => "COBOL",
+          languages => ["COBOL", "FORTRAN"]
         },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
+        alan_kay => {
+          known_for => "Object Orientation",
+          languages => ["Smalltalk", "LISP"]
         },
-        :dennis_ritchie => {
-          :known_for => "Unix",
+        dennis_ritchie => {
+          known_for => "Unix",
           :languages => ["C"]
         }
      }
